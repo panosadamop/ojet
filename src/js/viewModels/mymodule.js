e1b7b70420bbe3f54e31c3f0da709ad09e8c658a
padamopoulos <p.h.adamopoulos@gmail.com>
2019-02-08 11:17:50 +0200

    Tables

--- src/js/viewModels/mymodule.js
+++ src/js/viewModels/mymodule.js
@@ -6,40 +6,84 @@
 /*
  * Your about ViewModel code goes here
  */
-define(['ojs/ojcore', 'knockout', 'jquery'],
-    function(oj, ko, $) {
+define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojarraydataprovider', 'ojs/ojknockout', 'ojs/ojtable', 'ojs/ojgauge', 'ojs/ojchart', 'ojs/ojinputtext'],
+    function(oj, ko, $, ArrayDataProvider) {
 
         function MymoduleViewModel() {
             var self = this;
             // Below are a set of the ViewModel methods invoked by the oj-module component.
             // Please reference the oj-module jsDoc for additional information.
+            var deptArray = [{EmployeeId: 5, Photo: 'images/dvt/1.png',FirstName: 'Amy', LastName: 'Bartlet', Revenue: 100000, Rating: 1, TargetComplete: 20, TargetIncomplete: 80},
+                {EmployeeId: 10, Photo: 'images/dvt/10.png', FirstName: 'Andy', LastName: 'Jones', Revenue: 200000, Rating: 2, TargetComplete: 40, TargetIncomplete: 60},
+                {EmployeeId: 20, Photo: 'images/dvt/11.png', FirstName: 'Andrew', LastName: 'Bugsy', Revenue: 130000, Rating: 3, TargetComplete: 10, TargetIncomplete: 90},
+                {EmployeeId: 30, Photo: 'images/dvt/2.png', FirstName: 'Annette', LastName: 'Barnes', Revenue: 110000, Rating: 4, TargetComplete: 30, TargetIncomplete: 70},
+                {EmployeeId: 40, Photo: 'images/dvt/12.png', FirstName: 'Bob', LastName: 'Jones', Revenue: 230000, Rating: 5, TargetComplete: 70, TargetIncomplete: 30},
+                {EmployeeId: 50, Photo: 'images/dvt/13.png', FirstName: 'Bart', LastName: 'Buckler', Revenue: 400000, Rating: 1.5, TargetComplete: 90, TargetIncomplete: 10},
+                {EmployeeId: 60, Photo: 'images/dvt/14.png', FirstName: 'Bobby', LastName: 'Fisher', Revenue: 600000, Rating: 1.5, TargetComplete: 40, TargetIncomplete: 60}];
+            self.dataprovider = new ArrayDataProvider(deptArray, {keyAttributes: 'EmployeeId'});
 
-            /**
-             * Optional ViewModel method invoked after the View is inserted into the
-             * document DOM.  The application can put logic that requires the DOM being
-             * attached here.
-             * This method might be called multiple times - after the View is created
-             * and inserted into the DOM and after the View is reconnected
-             * after being disconnected.
-             */
-            self.connected = function() {
-                // Implement if needed
+            self.revenue_total_func = function(context)
+            {
+                var dataprovider = context.datasource;
+
+                if (!dataprovider)
+                {
+                    return;
+                }
+
+                var total = 0;
+                dataprovider.getTotalSize().then(function(totalRowCount) {
+                    var addRevenue = function(rowNum)
+                    {
+                        dataprovider.fetchByOffset({offset:rowNum}).then(function(value)
+                        {
+                            var row = value['results'][0];
+                            total = total + row['data']['Revenue'];
+                            if (rowNum < totalRowCount - 1)
+                            {
+                                addRevenue(rowNum + 1);
+                            }
+                            else
+                            {
+                                var parentElement = $(document.getElementById("table:revenue_total"));
+                                parentElement.attr('value', total);
+                            }
+                        });
+                    };
+                    addRevenue(0);
+                });
+                return total;
             };
 
-            /**
-             * Optional ViewModel method invoked after the View is disconnected from the DOM.
-             */
-            self.disconnected = function() {
-                // Implement if needed
-            };
+            self.columnArray = [{"renderer": oj.KnockoutTemplateUtils.getRenderer("emp_photo", true),
+                "footerRenderer": oj.KnockoutTemplateUtils.getRenderer("revenue_total_label", true),
+                "sortable": "disabled"},
+                {"headerText": "Employee Name",
+                    "sortable": "enabled",
+                    "renderer": oj.KnockoutTemplateUtils.getRenderer("emp_name", true),
+                    "sortProperty": "FirstName"},
+                {"headerText": "Sales Revenue",
+                    "renderer": oj.KnockoutTemplateUtils.getRenderer("revenue", true),
+                    "footerRenderer": oj.KnockoutTemplateUtils.getRenderer("revenue_total", true),
+                    "sortProperty": "Revenue"},
+                {"headerText": "Rating",
+                    "field": "Rating",
+                    "renderer": oj.KnockoutTemplateUtils.getRenderer("rating", true)},
+                {"headerText": "Sales Target Achievement",
+                    "field": "TargetAchievement",
+                    "sortable": "disabled",
+                    "renderer": oj.KnockoutTemplateUtils.getRenderer("target", true)}];
+        }
 
-            /**
-             * Optional ViewModel method invoked after transition to the new View is complete.
-             * That includes any possible animation between the old and the new View.
-             */
-            self.transitionCompleted = function() {
-                // Implement if needed
-            };
+        var vm = new viewModel;
+
+        $(document).ready
+        (
+            function()
+            {
+                ko.applyBindings(vm, document.getElementById('table'));
+            }
+        );
         }
 
         /*
